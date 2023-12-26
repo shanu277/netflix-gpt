32b399055a6e9bb2863e71f21d6ac8589087cbd1
shanu277 <shanu.mnit58@gmail.com>
2023-12-26 17:49:58 +0530

    loop video

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -8,7 +8,7 @@ const VideoContainer = ({ movieId }) => {
     <div className="w-screen">
       <iframe
       className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1&loop=1"}
         title="The Hunger Games: The Ballad of Songbirds &amp; Snakes (2023) Official Trailer 2"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
